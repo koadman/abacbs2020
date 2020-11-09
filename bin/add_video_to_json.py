fc7fc8ab88a8c84a31650759d4ac741ca58d2605
koadman <Aaron Darling>
2020-11-09 20:57:23 +1100

    [AD] leave title image blank when it failed to generate

--- bin/add_video_to_json.py
+++ bin/add_video_to_json.py
@@ -7,16 +7,19 @@ vid_urls = {}
 for line in video_links:
     d = line.rstrip().split()
     vid_urls[d[0]] = {}
-    vid_urls[d[0]]['video']=d[1]
-    vid_urls[d[0]]['image']=d[2]
+    vid_urls[d[0]]['video']=d[1]+"/download"
+    if len(d)>2:
+        vid_urls[d[0]]['image']=d[2]+"/download"
+    else:
+        vid_urls[d[0]]['image']=''
 
 for file in sys.argv[2:]:
     jfile = open(file, 'r')
     jdat = json.load(jfile)
     jfile.close()
     if jdat['number'] in vid_urls:
-        jdat['video_ID']=vid_urls[jdat['number']]['video'] + "/download"
-        jdat['video_stillframe']=vid_urls[jdat['number']]['image'] + "/download"
+        jdat['video_ID']=vid_urls[jdat['number']]['video']
+        jdat['video_stillframe']=vid_urls[jdat['number']]['image']
     else:
         for author in jdat['author_info']:
             if author['presenting']:
